b610e58a7991c41e9f163bf372cb3d6708570ff4
Georgits <georgi.tsertsvadze@gmail.com>
2018-09-01 11:51:12 +0200

    chapter4

--- 02_Python/09_Manipulating_DataFrames_with_pandas/09_Manipulating_DataFrames_with_pandas.py
+++ 02_Python/09_Manipulating_DataFrames_with_pandas/09_Manipulating_DataFrames_with_pandas.py
@@ -348,3 +348,218 @@ print(users_idx)
 kv_pairs = pd.melt(users_idx, col_level=0)
 # Print the key-value pairs
 print(kv_pairs)
+
+
+
+# Setting up a pivot table
+users = pd.read_csv('users.csv', index_col=0)
+# Create the DataFrame with the appropriate pivot table: by_city_day
+by_city_day = pd.pivot_table(users, index = 'weekday', columns='city')
+# Print by_city_day
+print(by_city_day)
+
+
+
+# Using other aggregations in pivot tables
+# Use a pivot table to display the count of each column: count_by_weekday1
+count_by_weekday1 = pd.pivot_table(users, index = 'weekday', columns='city', aggfunc='count')
+# Print count_by_weekday
+print(count_by_weekday1)
+# Replace 'aggfunc='count'' with 'aggfunc=len': count_by_weekday2
+count_by_weekday2 = pd.pivot_table(users, index = 'weekday', columns='city', aggfunc=len)
+# Verify that the same result is obtained
+print('==========================================')
+print(count_by_weekday1.equals(count_by_weekday2))
+
+
+# Use a pivot table to display the count of each column: count_by_weekday1
+count_by_weekday1 = users.pivot_table(index = 'weekday', aggfunc='count')
+# Print count_by_weekday
+print(count_by_weekday1)
+# Replace 'aggfunc='count'' with 'aggfunc=len': count_by_weekday2
+count_by_weekday2 = users.pivot_table(index = 'weekday', aggfunc=len)
+# Verify that the same result is obtained
+print('==========================================')
+print(count_by_weekday1.equals(count_by_weekday2))
+
+
+
+# Using margins in pivot tables
+# Create the DataFrame with the appropriate pivot table: signups_and_visitors
+signups_and_visitors = pd.pivot_table(users, index = 'weekday', aggfunc='sum')
+# Print signups_and_visitors
+print(signups_and_visitors)
+# Add in the margins: signups_and_visitors_total 
+signups_and_visitors_total = pd.pivot_table(users, index = 'weekday', aggfunc='sum', margins = True)
+# Print signups_and_visitors_total
+print(signups_and_visitors_total)
+
+
+
+
+
+
+
+
+
+
+# Chapter 4: Grouping data
+titanic = pd.read_csv('titanic.csv')
+# Grouping by multiple columns
+# Group titanic by 'pclass'
+by_class = titanic.groupby('pclass')
+# Aggregate 'survived' column of by_class by count
+count_by_class = by_class['survived'].count()
+# Print count_by_class
+print(count_by_class)
+# Group titanic by 'embarked' and 'pclass'
+by_mult = titanic.groupby(['embarked','pclass'])
+# Aggregate 'survived' column of by_mult by count
+count_mult = by_mult['survived'].count()
+# Print count_mult
+print(count_mult)
+
+
+
+# Grouping by another series
+life_fname = 'https://s3.amazonaws.com/assets.datacamp.com/production/course_1650/datasets/life_expectancy.csv'
+regions_fname = 'https://s3.amazonaws.com/assets.datacamp.com/production/course_1650/datasets/regions.csv'
+# Read life_fname into a DataFrame: life
+life = pd.read_csv(life_fname, index_col='Country')
+# Read regions_fname into a DataFrame: regions
+regions = pd.read_csv(regions_fname, index_col='Country')
+# Group life by regions['region']: life_by_region
+life_by_region = life.groupby(regions['region'])
+# Print the mean over the '2010' column of life_by_region
+print(life_by_region['2010'].mean())
+
+
+
+# Computing multiple aggregates of multiple columns
+# Group titanic by 'pclass': by_class
+by_class = titanic.groupby('pclass')
+# Select 'age' and 'fare'
+by_class_sub = by_class[['age','fare']]
+# Aggregate by_class_sub by 'max' and 'median': aggregated
+aggregated = by_class_sub.agg(['max','median'])
+# Print the maximum age in each class
+print(aggregated.loc[:, ('age','max')])
+# Print the median fare in each class
+print(aggregated.loc[:, ('fare','median')])
+
+
+
+# Aggregating on index levels/fields
+# Read the CSV file into a DataFrame and sort the index: gapminder
+gapminder = pd.read_csv('gapminder_tidy.csv', index_col=['Year','region','Country']).sort_index()
+# Group gapminder by 'Year' and 'region': by_year_region
+by_year_region = gapminder.groupby(['Year','region'])
+# Define the function to compute spread: spread
+def spread(series):
+    return series.max() - series.min()
+# Create the dictionary: aggregator
+aggregator = {'population':'sum', 'child_mortality':'mean', 'gdp':spread}
+# Aggregate by_year_region using the dictionary: aggregated
+aggregated = by_year_region.agg(aggregator)
+# Print the last 6 entries of aggregated 
+print(aggregated.tail(6))
+
+
+
+# Grouping on a function of the index
+# Read file: sales
+sales = pd.read_csv('sales-feb-2015.csv', index_col='Date', parse_dates=True)
+# Create a groupby object: by_day
+by_day = sales.groupby(sales.index.strftime('%a'))
+# Create sum: units_sum
+units_sum = by_day['Units'].sum()
+# Print units_sum
+print(units_sum)
+
+
+
+# Detecting outliers with Z-Scores
+# Import zscore
+from scipy.stats import zscore
+gapminder = pd.read_csv('gapminder_tidy.csv', index_col=['Year','region','Country']).sort_index()
+gapminder_2010 = gapminder.loc[(2010)]
+# Group gapminder_2010: standardized
+standardized = gapminder_2010.groupby('region')['life','fertility'].transform(zscore)
+# Construct a Boolean Series to identify outliers: outliers
+outliers = (standardized['life'] < -3) | (standardized['fertility'] > 3)
+# Filter gapminder_2010 by the outliers: gm_outliers
+gm_outliers = gapminder_2010.loc[outliers]
+# Print gm_outliers
+print(gm_outliers)
+
+
+
+# Filling missing data (imputation) by group
+titanic = pd.read_csv('titanic.csv')
+# Create a groupby object: by_sex_class
+by_sex_class = titanic.groupby(['sex','pclass'])
+# Write a function that imputes median
+def impute_median(series):
+    return series.fillna(series.median())
+# Impute age and assign to titanic['age']
+titanic.age = by_sex_class['age'].transform(impute_median)
+# Print the output of titanic.tail(10)
+print(titanic.tail(10))
+
+
+
+# Other transformations with .apply
+def disparity(gr):
+    # Compute the spread of gr['gdp']: s
+    s = gr['gdp'].max() - gr['gdp'].min()
+    # Compute the z-score of gr['gdp'] as (gr['gdp']-gr['gdp'].mean())/gr['gdp'].std(): z
+    z = (gr['gdp'] - gr['gdp'].mean())/gr['gdp'].std()
+    # Return a DataFrame with the inputs {'z(gdp)':z, 'regional spread(gdp)':s}
+    return pd.DataFrame({'z(gdp)':z , 'regional spread(gdp)':s})
+# Group gapminder_2010 by 'region': regional
+regional = gapminder_2010.groupby('region')
+# Apply the disparity function on regional: reg_disp
+reg_disp = regional.apply(disparity)
+# Print the disparity of 'United States', 'United Kingdom', and 'China'
+print(reg_disp.loc[['United States','United Kingdom','China'],:])
+
+
+
+
+# Grouping and filtering with .apply()
+# calculates the mean survival rates on the 'C' deck:
+def c_deck_survival(gr):
+    c_passengers = gr['cabin'].str.startswith('C').fillna(False)
+    return gr.loc[c_passengers, 'survived'].mean()
+# Create a groupby object using titanic over the 'sex' column: by_sex
+by_sex = titanic.groupby('sex')
+# Call by_sex.apply with the function c_deck_survival
+c_surv_by_sex = by_sex.apply(c_deck_survival)
+# Print the survival rates
+print(c_surv_by_sex)
+
+
+
+# Grouping and filtering with .filter()
+# Read the CSV file into a DataFrame: sales
+sales = pd.read_csv('sales-feb-2015.csv', index_col='Date', parse_dates=True)
+# Group sales by 'Company': by_company
+by_company = sales.groupby('Company')
+# Compute the sum of the 'Units' of by_company: by_com_sum
+by_com_sum =by_company['Units'].sum()
+print(by_com_sum)
+# Filter 'Units' where the sum is > 35: by_com_filt
+by_com_filt = by_company.filter(lambda g: g['Units'].sum() > 35)
+print(by_com_filt)
+
+
+
+# Filtering and grouping with .map()
+# Create the Boolean Series: under10
+under10 = (titanic['age'] < 10).map({True:'under 10', False:'over 10'})
+# Group by under10 and compute the survival rate
+survived_mean_1 = titanic.groupby(under10)['survived'].mean()
+print(survived_mean_1)
+# Group by under10 and pclass and compute the survival rate
+survived_mean_2 = titanic.groupby([under10,'pclass'])['survived'].mean()
+print(survived_mean_2)
