21f8aa69c8a742b3d165722fd75fdb8d15026cb5
Georgits <georgi.tsertsvadze@gmail.com>
2018-09-06 23:07:07 +0200

    chapter 4

--- 02_Python/10_Introduction_to_Databases_in_Python/10_Introduction_to_Databases_in_Python.py
+++ 02_Python/10_Introduction_to_Databases_in_Python/10_Introduction_to_Databases_in_Python.py
@@ -422,3 +422,158 @@ results_proxy.close()
 # Print the count by state
 print(state_count)
 
+
+
+
+
+
+
+
+
+
+# Chapter 4: 
+# Creating Tables with SQLAlchemy
+# Import Table, Column, String, Integer, Float, Boolean from sqlalchemy
+from sqlalchemy import Table, Column, String, Integer, Float, Boolean, create_engine, MetaData
+engine = create_engine('sqlite:///:memory:', echo=True)
+metadata = MetaData(bind = None)
+# Define a new table with a name, count, amount, and valid column: data
+data = Table('data', metadata,
+             Column('name', String(255)),
+             Column('count', Integer()),
+             Column('amount', Float()),
+             Column('valid', Boolean())
+)
+# Use the metadata to create the table
+metadata.create_all(engine)
+# Print table details
+print(repr(data))
+
+
+# Constraints and Data Defaults
+# Define a new table with a name, count, amount, and valid column: data
+data_2 = Table('data_2', metadata,
+             Column('name', String(255), unique=True),
+             Column('count', Integer(), default=1),
+             Column('amount', Float()),
+             Column('valid', Boolean(), default=False)
+)
+
+# Use the metadata to create the table
+metadata.create_all(engine)
+# Print the table details
+print(repr(metadata.tables['data_2']))
+
+
+# Inserting a single row with an insert() statement
+# Import insert and select from sqlalchemy
+from sqlalchemy import insert, select
+connection = engine.connect()
+# Build an insert statement to insert a record into the data table: stmt
+stmt = insert(data).values(name='Anna', count=1, amount=1000.00, valid=True)
+# Execute the statement via the connection: results
+results = connection.execute(stmt)
+# Print result rowcount
+print(results.rowcount)
+# Build a select statement to validate the insert
+stmt = select([data]).where(data.columns.name == 'Anna')
+# Print the result of executing the query.
+print(connection.execute(stmt).first())
+
+
+# Inserting Multiple Records at Once
+# Build a list of dictionaries: values_list
+values_list = [
+    {'name': 'Anna', 'count': 1, 'amount': 1000.00, 'valid': True},
+    {'name': 'Taylor', 'count': 1, 'amount': 750.00, 'valid': False},
+]
+# Build an insert statement for the data table: stmt
+stmt = insert(data)
+# Execute stmt with the values_list: results
+results = connection.execute(stmt, values_list)
+# Print rowcount
+print(results.rowcount)
+
+
+
+# Loading a CSV into a Table
+census = Table('census', MetaData(bind=None), 
+               Column('state', String(length=30)), 
+               Column('sex', String(length=1)), 
+               Column('age', Integer()), 
+               Column('pop2000', Integer()), 
+               Column('pop2008', Integer()), 
+               schema=None
+               )
+# Create a insert statement for census: stmt
+stmt = insert(census)
+# Create an empty list and zeroed row count: values_list, total_rowcount
+values_list = []
+total_rowcount = 0
+# Enumerate the rows of csv_reader
+import csv
+csvfile = open('census.csv', 'r')
+csv_reader =  csv.reader(csvfile)
+connection = engine.connect()
+for idx, row in enumerate(csv_reader):
+    #create data and append to values_list
+    data = {'state': row[0], 'sex': row[1], 'age': row[2], 'pop2000': row[3],
+            'pop2008': row[4]}
+    values_list.append(data)
+
+    # Check to see if divisible by 51
+    if idx % 51 == 0:
+        results = connection.execute(stmt, values_list)
+        total_rowcount += results.rowcount
+        values_list = []
+# Print total rowcount
+print(total_rowcount)
+
+
+
+# Updating individual records
+# !!! NICHT LAUFFÄHIG
+# Build a select statement: select_stmt
+select_stmt = select([state_fact]).where(state_fact.columns.name == 'New York')
+# Print the results of executing the select_stmt
+print(connection.execute(select_stmt).fetchall())
+# Build a statement to update the fips_state to 36: stmt
+stmt = update(state_fact).values(fips_state = 36)
+# Append a where clause to limit it to records for New York state
+stmt = stmt.where(state_fact.columns.name == 'New York')
+# Execute the statement: results
+results = connection.execute(stmt)
+# Print rowcount
+print(results.rowcount)
+# Execute the select_stmt again to view the changes
+print(connection.execute(select_stmt).fetchall())
+
+
+
+
+# Updating Multiple Records
+# !!! NICHT LAUFFÄHIG
+# Build a statement to update the notes to 'The Wild West': stmt
+stmt = update(state_fact).values(notes='The Wild West')
+# Append a where clause to match the West census region records
+stmt = stmt.where(state_fact.columns.census_region_name == 'West')
+# Execute the statement: results
+results = connection.execute(stmt)
+# Print rowcount
+print(results.rowcount)
+
+
+
+# Correlated Updates
+# !!! NICHT LAUFFÄHIG
+# Build a statement to select name from state_fact: stmt
+fips_stmt = select([state_fact.columns.name])
+# Append a where clause to Match the fips_state to flat_census fips_code
+fips_stmt = fips_stmt.where(
+    state_fact.columns.fips_state == flat_census.columns.fips_code)
+# Build an update statement to set the name to fips_stmt: update_stmt
+update_stmt = update(flat_census).values(state_name=fips_stmt)
+# Execute update_stmt: results
+results = connection.execute(update_stmt)
+# Print rowcount
+print(results.rowcount)
