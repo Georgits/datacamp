08c5414a6d4ab7cc93ec806a232e63d1990ea222
Georgits <georgi.tsertsvadze@gmail.com>
2018-09-07 15:49:18 +0200

    chapter 4 completed

--- 02_Python/10_Introduction_to_Databases_in_Python/10_Introduction_to_Databases_in_Python.py
+++ 02_Python/10_Introduction_to_Databases_in_Python/10_Introduction_to_Databases_in_Python.py
@@ -577,3 +577,57 @@ update_stmt = update(flat_census).values(state_name=fips_stmt)
 results = connection.execute(update_stmt)
 # Print rowcount
 print(results.rowcount)
+
+
+
+# Deleting all the records from a table
+# !!! NICHT LAUFFÄHIG
+# Import delete, select
+from sqlalchemy import delete, select
+# Build a statement to empty the census table: stmt
+stmt = delete(census)
+# Execute the statement: results
+results = connection.execute(stmt)
+# Print affected rowcount
+print(results.rowcount)
+# Build a statement to select all records from the census table
+stmt = select([census])
+# Print the results of executing the statement to verify there are no rows
+print(connection.execute(stmt).fetchall())
+
+
+
+# Deleting specific records
+# !!! NICHT LAUFFÄHIG
+# Build a statement to count records using the sex column for Men ('M') age 36: stmt
+stmt = select([func.count(census.columns.sex)]).where(
+    and_(census.columns.sex == 'M',
+         census.columns.age == 36)
+)
+# Execute the select statement and use the scalar() fetch method to save the record count
+to_delete = connection.execute(stmt).scalar()
+# Build a statement to delete records from the census table: stmt_del
+stmt_del = delete(census)
+# Append a where clause to target Men ('M') age 36
+stmt_del = stmt_del.where(
+    and_(census.columns.sex == 'M',
+         census.columns.age == 36)
+)
+# Execute the statement: results
+results = connection.execute(stmt_del)
+# Print affected rowcount and to_delete record count, make sure they match
+print(results.rowcount, to_delete)
+
+
+
+# Deleting a Table Completely
+engine = create_engine('sqlite:///census.sqlite')
+metadata = MetaData(bind=None)
+# Drop the state_fact table
+state_fact.drop(engine)
+# Check to see if state_fact exists
+print(state_fact.exists(engine))
+# Drop all tables
+metadata.drop_all(engine)
+# Check to see if census exists
+print(census.exists(engine))
