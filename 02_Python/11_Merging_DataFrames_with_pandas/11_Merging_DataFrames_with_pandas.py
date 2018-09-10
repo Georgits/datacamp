ad952f43e2b5e001a684f6db4da8cdeec6921ad2
Georgits <georgi.tsertsvadze@gmail.com>
2018-09-10 23:25:01 +0200

    chapter 2

--- 02_Python/11_Merging_DataFrames_with_pandas/11_Merging_DataFrames_with_pandas.py
+++ 02_Python/11_Merging_DataFrames_with_pandas/11_Merging_DataFrames_with_pandas.py
@@ -11,7 +11,6 @@ import matplotlib.pyplot as plt
 # path = 'C:\\Users\\d91067\\Desktop\\R\\datacamp\\02_Python\\10_Introduction_to_Databases_in_Python'
 path = 'C:\\Users\\georg\\Desktop\\georgi\\github\\datacamp\\02_Python\\11_Merging_DataFrames_with_pandas'
 os.chdir(path)
-0176-65346140 
 
 # Chapter 1: Preparing data
 # Reading DataFrames from multiple files
@@ -155,3 +154,84 @@ print(dollars.head(5))
 pounds = dollars.multiply(exchange['GBP/USD'], axis = 'rows')
 # Print the head of pounds
 print(pounds.head())
+
+
+
+
+
+
+
+
+
+
+# Chapter 2: Concatenating data
+# Appending pandas Series
+# Load 'sales-jan-2015.csv' into a DataFrame: jan
+jan = pd.read_csv('sales-jan-2015.csv', parse_dates = True, index_col = 'Date')
+# Load 'sales-feb-2015.csv' into a DataFrame: feb
+feb = pd.read_csv('sales-feb-2015.csv', parse_dates = True, index_col = 'Date')
+# Load 'sales-mar-2015.csv' into a DataFrame: mar
+mar = pd.read_csv('sales-mar-2015.csv', parse_dates = True, index_col = 'Date')
+# Extract the 'Units' column from jan: jan_units
+jan_units = jan['Units']
+# Extract the 'Units' column from feb: feb_units
+feb_units = feb['Units']
+# Extract the 'Units' column from mar: mar_units
+mar_units = mar['Units']
+# Append feb_units and then mar_units to jan_units: quarter1
+quarter1 = jan_units.append(feb_units).append(mar_units)
+# Print the first slice from quarter1
+print(quarter1.loc['jan 27, 2015':'feb 2, 2015'])
+# Print the second slice from quarter1
+print(quarter1.loc['feb 26, 2015':'mar 7, 2015'])
+# Compute & print total sales in quarter1
+print(quarter1.sum())
+
+
+
+# Concatenating pandas Series along row axis
+# Initialize empty list: units
+units = []
+# Build the list of Series
+for month in [jan, feb, mar]:
+    print(month['Units'])
+    units.append(month['Units'])
+# Concatenate the list: quarter1
+quarter1 = pd.concat(units, axis = 'rows')
+# Print slices from quarter1
+print(quarter1.loc['jan 27, 2015':'feb 2, 2015'])
+print(quarter1.loc['feb 26, 2015':'mar 7, 2015'])
+
+
+
+# Appending DataFrames with ignore_index
+names_1881 =pd.read_csv('names1881.csv', index_col = 0 )
+names_1981 = pd.read_csv('names1981.csv', index_col = 0 )
+names_1881['year'] = 1881
+names_1981['year'] = 1981
+# Append names_1981 after names_1881 with ignore_index=True: combined_names
+combined_names = names_1881.append(names_1981, ignore_index = True)
+# Print shapes of names_1981, names_1881, and combined_names
+print(names_1981.shape)
+print(names_1881.shape)
+
+
+pd.read_csv('names_1881.csv', index_col = 0 )names_1881['year'] = 1881
+names_1981['year'] = 1981
+# Append names_1981 after names_1881 with ignore_index=True: combined_names
+combined_names = names_1881.append(names_1981, ignore_index = True)
+# Print shapes of names_1981, names_1881, and combined_names
+print(names_1981.shape)
+print(names_1881.shape)
+print(combined_names.shape)
+# Print all rows that contain the name 'Morgan'
+print(combined_names.loc[combined_names['name'] =='Morgan'])
+
+
+
+# Concatenating pandas DataFrames along column axis
+### NICHT LAUFFÄHIG !!!!
+# Concatenate weather_max and weather_mean horizontally: weather
+weather = pd.concat([weather_max, weather_mean], axis = 1)
+# Print weather
+print(weather)
