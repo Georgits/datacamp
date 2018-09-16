9bebccb5b01b3ef813d152b33431e041075e9320
Georgits <georgi.tsertsvadze@gmail.com>
2018-09-16 21:51:29 +0200

    course 12, chapter 3

--- 02_Python/12_Introduction_to_Data_Visualization_with_Python/12_Introduction_to_Data_Visualization_with_Python.py
+++ 02_Python/12_Introduction_to_Data_Visualization_with_Python/12_Introduction_to_Data_Visualization_with_Python.py
@@ -8,6 +8,7 @@ import os
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import seaborn as sns
 # path = 'C:\\Users\\d91067\\Desktop\\R\\datacamp\\02_Python\\12_Introduction_to_Data_Visualization_with_Python'
 path = 'C:\\Users\\georg\\Desktop\\georgi\\github\\datacamp\\02_Python\\12_Introduction_to_Data_Visualization_with_Python'
 os.chdir(path)
@@ -388,3 +389,149 @@ plt.imshow((rescaled_image*255).astype(np.uint8))
 plt.show()
 # Läuft nicht 100% so wie es laufen müsste
 # https://stackoverflow.com/questions/49643907/clipping-input-data-to-the-valid-range-for-imshow-with-rgb-data-0-1-for-floa
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Chapter 3: Statistical plots with Seaborn
+# Simple linear regressions
+auto = pd.read_csv('auto-mpg.csv')
+# Plot a linear regression between 'weight' and 'hp'
+sns.lmplot(x = 'weight', y = 'hp', data=auto)
+# Display the plot
+plt.show()
+
+# Plotting residuals of a regression
+# Generate a green residual plot of the regression between 'hp' and 'mpg'
+sns.residplot(x='hp', y='mpg', data=auto, color='green')
+# Display the plot
+plt.show()
+
+# Higher-order regressions
+# Generate a scatter plot of 'weight' and 'mpg' using red circles
+plt.scatter(auto['weight'], auto['mpg'], label='data', color='red', marker='o')
+# Plot in blue a linear regression of order 1 between 'weight' and 'mpg'
+sns.regplot(x='weight', y='mpg', data=auto, color='blue', scatter=None, label='order 1')
+# Plot in green a linear regression of order 2 between 'weight' and 'mpg'
+sns.regplot(x='weight', y='mpg', data=auto, color='green', scatter=None, label='order 2', order = 2)
+# Add a legend and display the plot
+plt.legend(loc = 'upper right')
+plt.show()
+
+
+# Grouping linear regressions by hue
+# Plot a linear regression between 'weight' and 'hp', with a hue of 'origin' and palette of 'Set1'
+sns.lmplot(x = 'weight', y = 'hp', data = auto, hue = 'origin', palette = 'Set1')
+# Display the plot
+plt.style.use('ggplot')
+plt.show()
+
+# Grouping linear regressions by row or column
+# Plot linear regressions between 'weight' and 'hp' grouped row-wise by 'origin'
+sns.lmplot(x = 'weight', y = 'hp', data = auto, row = 'origin', palette = 'Set1')
+# Display the plot
+plt.show()
+
+
+# Constructing strip plots
+# Make a strip plot of 'hp' grouped by 'cyl'
+plt.subplot(2,1,1)
+sns.stripplot(x='cyl', y='hp', data=auto)
+# Make the strip plot again using jitter and a smaller point size
+plt.subplot(2,1,2)
+sns.stripplot(x='cyl', y='hp', data=auto, jitter = True, size = 3)
+# Display the plot
+plt.show()
+
+
+# Constructing swarm plots
+# Generate a swarm plot of 'hp' grouped horizontally by 'cyl'  
+plt.subplot(2,1,1)
+sns.swarmplot(x='cyl', y='hp', data=auto)
+# Generate a swarm plot of 'hp' grouped vertically by 'cyl' with a hue of 'origin'
+plt.subplot(2,1,2)
+sns.swarmplot(x='hp', y='cyl', data=auto, hue='origin', orient = 'h')
+# Display the plot
+plt.show()
+
+
+# Constructing violin plots
+# Generate a violin plot of 'hp' grouped horizontally by 'cyl'
+plt.subplot(2,1,1)
+sns.violinplot(x='cyl', y='hp', data=auto)
+# Generate the same violin plot again with a color of 'lightgray' and without inner annotations
+plt.subplot(2,1,2)
+sns.violinplot(x='cyl', y='hp', data=auto, color = 'lightgray', inner = None)
+# Overlay a strip plot on the violin plot
+sns.stripplot(x='cyl', y='hp', data=auto, jitter = True, size = 1.5)
+# Display the plot
+plt.show()
+
+
+# Plotting joint distributions (1)
+# Generate a joint plot of 'hp' and 'mpg'
+sns.jointplot(x='hp', y='mpg', data = auto)
+# Display the plot
+plt.show()
+
+
+# Plotting joint distributions (2)
+# The seaborn function sns.jointplot() has a parameter kind to specify how to visualize 
+# the joint variation of two continuous random variables (i.e., two columns of a DataFrame)
+
+#    kind='scatter' uses a scatter plot of the data points
+#    kind='reg' uses a regression plot (default order 1)
+#    kind='resid' uses a residual plot
+#    kind='kde' uses a kernel density estimate of the joint distribution
+#    kind='hex' uses a hexbin plot of the joint distribution
+# Generate a joint plot of 'hp' and 'mpg' using a hexbin plot
+sns.jointplot(x='hp', y='mpg', data=auto, kind = 'hex')
+# Display the plot
+plt.show()
+
+
+# Plotting distributions pairwise (1)
+# Print the first 5 rows of the DataFrame
+print(auto.head())
+# Plot the pairwise joint distributions from the DataFrame 
+sns.pairplot(auto)
+# Display the plot
+plt.show()
+
+
+# Plotting distributions pairwise (2)
+# You will display regressions as well as scatter plots in the off-diagonal subplots. You will do this with the argument kind='reg' (where 'reg' means 'regression'). Another option for kind is 'scatter' (the default) that plots scatter plots in the off-diagonal subplots.
+# You will also visualize the joint distributions separated by continent of origin. You will do this with the keyword argument hue specifying the 'origin'.
+# Print the first 5 rows of the DataFrame
+print(auto.head())
+# Plot the pairwise joint distributions grouped by 'origin' along with regression lines
+sns.pairplot(auto, hue = 'origin', kind = 'reg')
+# Display the plot
+plt.show()
+# Es werden keine Werte gezeichnet
+
+
+# Visualizing correlations with a heatmap
+cov_matrix = auto.corr()
+# Print the covariance matrix
+print(cov_matrix)
+# Visualize the covariance matrix using a heatmap
+sns.heatmap(cov_matrix)
+# Display the heatmap
+plt.show()
+
+
+
