49417a846aafe64280120b3ab2d100191483db41
Georgits <georgi.tsertsvadze@gmail.com>
2018-09-20 23:29:11 +0200

    COURSE 13, chapter 2 completed

--- 02_Python/13_Interactive_Data_Visualization_with_Bokeh/13_Interactive_Data_Visualization_with_Bokeh.py
+++ 02_Python/13_Interactive_Data_Visualization_with_Bokeh/13_Interactive_Data_Visualization_with_Bokeh.py
@@ -23,21 +23,37 @@ from bokeh.plotting import ColumnDataSource
 from bokeh.models import HoverTool
 #Import CategoricalColorMapper from bokeh.models
 from bokeh.models import CategoricalColorMapper
+# Import row from bokeh.layouts
+from bokeh.layouts import row, column
+# # Import gridplot from bokeh.layouts
+from bokeh.layouts import gridplot
+# Import Panel from bokeh.models.widgets
+from bokeh.models.widgets import Panel, Tabs
+
+
+literacy_birth_rate = pd.read_csv('literacy_birth_rate.csv')
+literacy_birth_rate = literacy_birth_rate.iloc[:162,:]
+fertility = pd.to_numeric(literacy_birth_rate['fertility'])
+female_literacy = pd.to_numeric(literacy_birth_rate['female_literacy'])
+population = pd.to_numeric(literacy_birth_rate['population'])
+
+literacy_birth_rate['fertility'] = pd.to_numeric(literacy_birth_rate['fertility'])
+literacy_birth_rate['female_literacy'] = pd.to_numeric(literacy_birth_rate['female_literacy'])
+literacy_birth_rate['population'] = pd.to_numeric(literacy_birth_rate['population'])
+
+fertility_latinamerica = pd.to_numeric(literacy_birth_rate[literacy_birth_rate['Continent'] == 'LAT']['fertility'])
+female_literacy_latinamerica = pd.to_numeric(literacy_birth_rate[literacy_birth_rate['Continent'] == 'LAT']['female_literacy'])
+
+fertility_africa = pd.to_numeric(literacy_birth_rate[literacy_birth_rate['Continent'] == 'AF']['fertility'])
+female_literacy_africa = pd.to_numeric(literacy_birth_rate[literacy_birth_rate['Continent'] == 'AF']['female_literacy'])
+
+
+latin_america = literacy_birth_rate[literacy_birth_rate['Continent'] == 'LAT']
+africa = literacy_birth_rate[literacy_birth_rate['Continent'] == 'AF']
 
 
 # Chapter 1:  Basic plotting with Bokeh
 # A simple scatter plot
-literacy_birth_rate = pd.read_csv('literacy_birth_rate.csv')
-literacy_birth_rate = literacy_birth_rate.iloc[:162,:]
-fertility = pd.to_numeric(literacy_birth_rate['fertility'])
-female_literacy = pd.to_numeric(literacy_birth_rate['female literacy'])
-
-fertility_latinamerica = pd.to_numeric(literacy_birth_rate[literacy_birth_rate['Continent'] == 'LAT']['fertility'])
-female_literacy_latinamerica = pd.to_numeric(literacy_birth_rate[literacy_birth_rate['Continent'] == 'LAT']['female literacy'])
-
-fertility_africa = pd.to_numeric(literacy_birth_rate[literacy_birth_rate['Continent'] == 'AF']['fertility'])
-female_literacy_africa = pd.to_numeric(literacy_birth_rate[literacy_birth_rate['Continent'] == 'AF']['female literacy'])
-
 # Create the figure: p
 p = figure(x_axis_label='fertility (children per woman)', y_axis_label='female_literacy (% population)')
 # Add a circle glyph to the figure p
@@ -202,4 +218,192 @@ p.circle('weight', 'mpg', source=source,
             legend='origin')
 # Specify the name of the output file and show the result
 output_file('colormap.html')
+show(p)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Chapter 2: Layouts, Interactions, and Annotations
+# Creating rows of plots
+# Convert df to a ColumnDataSource: source
+source = ColumnDataSource(literacy_birth_rate)
+# Create the first figure: p1
+p1 = figure(x_axis_label='fertility (children per woman)', y_axis_label='female_literacy (% population)')
+# Add a circle glyph to p1
+p1.circle('fertility', 'female_literacy', source=source)
+# Create the second figure: p2
+p2 = figure(x_axis_label='population', y_axis_label='female_literacy (% population)')
+# Add a circle glyph to p2
+p2.circle('population', 'female_literacy', source=source)
+# Put p1 and p2 into a horizontal row: layout
+layout = row(p1,p2)
+# Specify the name of the output_file and show the result
+output_file('fert_row.html')
+show(layout)
+
+
+
+
+# Creating columns of plots
+# Create a blank figure: p1
+p1 = figure(x_axis_label='fertility (children per woman)', y_axis_label='female_literacy (% population)')
+# Add circle scatter to the figure p1
+p1.circle('fertility', 'female_literacy', source=source)
+# Create a new blank figure: p2
+p2 = figure(x_axis_label='population', y_axis_label='female_literacy (% population)')
+# Add circle scatter to the figure p2
+p2.circle('population', 'female_literacy', source=source)
+# Put plots p1 and p2 in a column: layout
+layout = column(p1,p2)
+# Specify the name of the output_file and show the result
+output_file('fert_column.html')
+show(layout)
+
+
+
+# Nesting rows and columns of plots
+df = pd.read_csv('auto-mpg.csv')
+avg_mpg = df.groupby(['yr'])['mpg'].mean()
+avg_mpg = pd.DataFrame(avg_mpg)
+# avg_mpg.columns = ['avg_mpg']
+source = ColumnDataSource(df)
+source_avg = ColumnDataSource(avg_mpg)
+
+
+mpg_hp = figure(x_axis_label='hp', y_axis_label='mpg')
+mpg_hp.circle('hp', 'mpg', source = source)
+
+
+mpg_weight = figure(x_axis_label='weight', y_axis_label='mpg')
+mpg_weight.circle('weight', 'mpg', source = source)
+
+avg_mpg = figure(x_axis_label='Year', y_axis_label='avg_mpg')
+avg_mpg.line('yr', 'mpg', source = source_avg)
+
+
+# Make a column layout that will be used as the second row: row2
+row2 = column([mpg_hp, mpg_weight], sizing_mode='scale_width')
+# Make a row layout that includes the above column layout: layout
+layout = row([avg_mpg, row2], sizing_mode='scale_width')
+# Specify the name of the output_file and show the result
+output_file('layout_custom.html')
+show(layout)
+
+
+
+# Creating gridded layouts
+# Create a list containing plots p1 and p2: row1
+row1 = [p1,p2]
+# Create a list containing plots p3 and p4: row2
+row2 = [mpg_hp,mpg_weight]
+# Create a gridplot using row1 and row2: layout
+layout = gridplot([row1, row2])
+# Specify the name of the output_file and show the result
+output_file('grid.html')
+show(layout)
+
+
+
+# Starting tabbed layouts
+# Create tab1 from plot p1: tab1
+tab1 = Panel(child=mpg_hp, title='Latin America')
+# Create tab2 from plot p2: tab2
+tab2 = Panel(child=mpg_weight, title='Africa')
+# Create tab3 from plot p3: tab3
+tab3 = Panel(child=avg_mpg, title='Asia')
+# Create tab4 from plot p4: tab4
+tab4 = Panel(child=mpg_hp, title='Europe')
+
+
+
+# Displaying tabbed layouts
+### LÄUFT NICHT
+# Create a Tabs layout: layout
+layout = Tabs(tabs=[tab1, tab2, tab3, tab4])
+# Specify the name of the output_file and show the result
+output_file('tabs.html')
+show(layout)
+
+
+# Linked axes
+### LÄUFT NICHT
+# Link the x_range of p2 to p1: p2.x_range
+p2.x_range = p1.x_range
+# Link the y_range of p2 to p1: p2.y_range
+p2.y_range = p1.y_range
+# Link the x_range of p3 to p1: p3.x_range
+p3.x_range = p1.x_range
+# Link the y_range of p4 to p1: p4.y_range
+p4.y_range = p1.y_range
+# Specify the name of the output_file and show the result
+output_file('linked_range.html')
+show(layout)
+
+
+
+# Linked brushing
+# Create ColumnDataSource: source
+source = ColumnDataSource(literacy_birth_rate)
+# Create the first figure: p1
+p1 = figure(x_axis_label='fertility (children per woman)', y_axis_label='female literacy (% population)', tools='box_select,lasso_select')
+# Add a circle glyph to p1
+p1.circle('fertility', 'female_literacy', source=source)
+# Create the second figure: p2
+p2 = figure(x_axis_label='fertility (children per woman)', y_axis_label='population (millions)', tools='box_select,lasso_select')
+# Add a circle glyph to p2
+p2.circle('fertility', 'population', source=source)
+# Create row layout of figures p1 and p2: layout
+layout = row(p1,p2)
+# Specify the name of the output_file and show the result
+output_file('linked_brush.html')
+show(layout)
+
+
+
+# How to create legends
+latin_america = ColumnDataSource(latin_america)
+africa = ColumnDataSource(africa)
+p = figure(x_axis_label='fertility', y_axis_label='female_literacy (% population)')
+# Add the first circle glyph to the figure p
+p.circle('fertility', 'female_literacy', source=latin_america, size=10, color='red', legend='Latin America')
+# Add the second circle glyph to the figure p
+p.circle('fertility', 'female_literacy', source=africa, size=10, color='blue', legend='Africa')
+# Specify the name of the output_file and show the result
+output_file('fert_lit_groups.html')
+show(p)
+
+
+#Positioning and styling legends
+# Assign the legend to the bottom left: p.legend.location
+p.legend.location = 'bottom_left'
+# Fill the legend background with the color 'lightgray': p.legend.background_fill_color
+p.legend.background_fill_color = 'lightgray'
+# Specify the name of the output_file and show the result
+output_file('fert_lit_groups.html')
+show(p)
+
+
+
+# Adding a hover tooltip
+# Create a HoverTool object: hover
+hover = HoverTool(tooltips = [('Country', '@Country')])
+# Add the HoverTool object to figure p
+p.add_tools(hover)
+# Specify the name of the output_file and show the result
+output_file('hover.html')
 show(p)
