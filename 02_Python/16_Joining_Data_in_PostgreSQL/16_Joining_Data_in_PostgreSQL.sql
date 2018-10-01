3916bc9d329c4cac8f00a1f5dd4d1bf5d0439927
Georgits <georgi.tsertsvadze@gmail.com>
2018-10-01 17:53:45 +0200

    course 16 , chapter 3 completed

--- 02_Python/16_Joining_Data_in_PostgreSQL/16_Joining_Data_in_PostgreSQL.sql
+++ 02_Python/16_Joining_Data_in_PostgreSQL/16_Joining_Data_in_PostgreSQL.sql
@@ -74,3 +74,287 @@ INNER JOIN pop_plus AS p
 ON c.code = p.country_code
 ORDER BY geosize_group;
 
+
+
+
+
+
+-- Chapter 2. Outer joins and cross joins
+
+-- Left Join --
+-- get the city name (and alias it), the country code,
+-- the country name (and alias it), the region,
+-- and the city proper population
+SELECT c1.name AS city, code, c2.name AS country,
+       region, city_proper_pop
+-- specify left table
+FROM cities AS c1
+-- specify right table and type of join
+INNER JOIN countries AS c2
+-- how should the tables be matched?
+ON c1.country_code = c2.code
+-- sort based on descending country code
+ORDER BY code DESC;
+
+-- Change the code to perform a LEFT JOIN instead of an INNER JOIN. After executing this query, note how many records the query result contains.
+
+-- get the city name (and alias it), the country code,
+-- the country name (and alias it), the region,
+-- and the city proper population
+SELECT c1.name AS city, code, c2.name AS country,
+       region, city_proper_pop
+-- specify left table
+FROM cities AS c1
+-- specify right table and type of join
+LEFT JOIN countries AS c2
+-- how should the tables be matched?
+ON c1.country_code = c2.code
+-- sort based on descending country code
+ORDER BY code DESC;
+
+
+-- Left join (2)
+/*
+select country name AS country, the country's local name,
+the language name AS language, and
+the percent of the language spoken in the country
+*/
+SELECT c.name AS country, local_name, l.name AS language, percent
+-- countries on the left (alias as c)
+FROM countries AS c
+-- appropriate join with languages (as l) on the right
+LEFT JOIN languages AS l
+-- give fields to match on
+ON c.code = l.code
+-- sort by descending country name
+ORDER BY c.name DESC;
+
+
+-- Left join (3)
+-- select name, region, and gdp_percapita
+SELECT c.name, region, gdp_percapita
+-- from countries (alias c) on the left
+FROM countries AS c
+-- left join with economies (alias e)
+LEFT JOIN economies AS e
+-- match on code fields
+ON c.code = e.code
+-- focus on 2010 entries
+WHERE year = 2010;
+
+-- Select region, average gdp_percapita (alias avg_gdp)
+SELECT region, avg(gdp_percapita) AS avg_gdp
+-- From countries (alias c) on the left
+FROM countries AS c
+-- Join with economies (alias e)
+LEFT JOIN economies AS e
+-- Match on code fields
+ON c.code = e.code
+-- Focus on 2010 
+WHERE year = 2010
+-- Group by region
+GROUP BY region
+-- Order by avg_gdp, descending
+ORDER BY avg_gdp DESC;
+
+
+-- Right join
+-- convert this code to use RIGHT JOINs instead of LEFT JOINs
+/*
+SELECT cities.name AS city, urbanarea_pop, countries.name AS country,
+       indep_year, languages.name AS language, percent
+FROM cities
+LEFT JOIN countries
+ON cities.country_code = countries.code
+LEFT JOIN languages
+ON countries.code = languages.code
+ORDER BY city, language;
+*/
+
+SELECT cities.name AS city, urbanarea_pop, countries.name AS country,
+       indep_year, languages.name AS language, percent
+FROM languages
+RIGHT JOIN countries
+ON languages.code = countries.code
+RIGHT JOIN cities
+ON countries.code = cities.country_code
+ORDER BY city, language;
+
+
+
+-- Full join
+SELECT name AS country, code, region, basic_unit
+FROM countries
+FULL JOIN currencies
+USING (code)
+WHERE region = 'North America' OR region IS NULL
+ORDER BY region;
+
+
+-- Full join (2)
+-- Choose records in which countries.name starts with the capital letter 'V' or is NULL. 
+SELECT countries.name, code, languages.name AS language
+FROM languages
+FULL JOIN countries
+USING (code)
+WHERE countries.name LIKE 'V%' OR countries.name IS NULL
+ORDER BY countries.name;
+
+
+-- Full join (3)
+SELECT c1.name AS country, region, l.name AS language,
+       basic_unit, 	frac_unit
+FROM countries AS c1
+FULL JOIN languages AS l
+USING (code)
+FULL JOIN currencies AS c2
+USING (code)
+WHERE region LIKE 'M%esia';
+
+
+-- A table of two cities
+SELECT c.name AS city, l.name AS language
+FROM cities AS c        
+CROSS JOIN languages  AS l
+WHERE c.name LIKE 'Hyder%';
+
+SELECT c.name AS city, l.name AS language
+FROM cities AS c        
+INNER JOIN languages  AS l
+ON c.country_code = l.code
+WHERE c.name LIKE 'Hyder%';
+
+
+-- Outer challenge
+SELECT c.name AS country, region, life_expectancy AS life_exp
+FROM countries AS c
+LEFT JOIN populations AS p
+ON c.code = p.	country_code
+WHERE year = 2010 
+ORDER BY life_exp
+LIMIT 5;
+
+
+
+
+-- CHAPTER 3: Set theory clauses
+-- Union
+-- pick specified columns from 2010 table
+SELECT  *
+-- 2010 table will be on top
+FROM  economies2010
+-- which set theory clause?
+UNION
+-- pick specified columns from 2015 table
+SELECT  * 
+-- 2015 table on the bottom
+FROM  economies2015
+-- order accordingly
+ORDER BY code, year;
+
+
+-- Union (2)
+SELECT country_code
+FROM cities
+UNION
+SELECT code AS country_code
+FROM currencies
+ORDER BY country_code;
+
+
+-- Union all
+SELECT code, year
+FROM economies
+UNION ALL
+SELECT country_code, year
+FROM populations
+ORDER BY code, year;
+
+
+-- Intersect
+SELECT code, year
+FROM economies
+INTERSECT
+SELECT country_code, year
+FROM populations
+ORDER BY code, year;
+
+
+-- Intersect (2)
+SELECT name
+FROM countries
+INTERSECT
+SELECT name
+FROM cities;
+
+
+
+-- Except
+SELECT name
+FROM cities
+EXCEPT
+SELECT capital
+FROM countries
+ORDER BY name;
+
+
+
+-- Except (2)
+SELECT capital
+FROM countries
+EXCEPT
+SELECT name
+FROM cities
+ORDER bY capital;
+
+
+-- Semi-join
+SELECT DISTINCT name
+FROM languages
+WHERE code IN
+    (SELECT code
+    FROM countries
+    WHERE region = 'Middle East')
+ORDER BY name;
+
+
+-- Diagnosing problems using anti-join
+SELECT COUNT(*)
+FROM countries
+WHERE continent = 'Oceania';
+
+SELECT c1.code, name, basic_unit AS currency
+FROM countries AS c1
+INNER JOIN currencies AS c2
+ON c1.code = c2.code
+WHERE c1.continent = 'Oceania';
+
+SELECT code, name
+FROM countries
+WHERE continent = 'Oceania'
+    AND code NOT IN
+    (SELECT code 
+     FROM currencies);
+
+	 
+	 
+-- Set theory challenge
+-- select the city name
+SELECT name
+-- alias the table where city name resides
+FROM cities AS c1
+-- choose only records matching the result of multiple set theory clauses
+WHERE country_code IN
+(
+    -- select appropriate field from economies AS e
+    SELECT e.code
+    FROM economies AS e
+    -- get all additional (unique) values of the field from currencies AS c2  
+    UNION
+    SELECT DISTINCT c2.code
+    FROM currencies AS c2
+    -- exclude those appearing in populations AS p
+    EXCEPT
+    SELECT p.	country_code
+    FROM populations AS p
+);
