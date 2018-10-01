8164092cc9c92ea2a671e50a68ac81a15a852abf
Georgits <georgi.tsertsvadze@gmail.com>
2018-10-02 00:17:23 +0200

    course 16, Zwischenstand

--- 02_Python/16_Joining_Data_in_PostgreSQL/16_Joining_Data_in_PostgreSQL.sql
+++ 02_Python/16_Joining_Data_in_PostgreSQL/16_Joining_Data_in_PostgreSQL.sql
@@ -358,3 +358,113 @@ WHERE country_code IN
     SELECT p.	country_code
     FROM populations AS p
 );
+
+
+
+-- CHAPTER 4: Subqueries
+-- Subquery inside where
+SELECT AVG(life_expectancy)
+FROM populations
+WHERE year = 2015;
+
+SELECT *
+FROM populations
+WHERE life_expectancy > 1.15 * (
+    SELECT AVG(life_expectancy)
+    FROM populations
+    WHERE year = 2015)
+    AND year = 2015;
+
+	
+-- Subquery inside where (2)
+-- select the appropriate fields
+SELECT name, country_code, urbanarea_pop
+-- from the cities table
+FROM cities
+-- with city name in the field of capital cities
+WHERE name IN
+  (SELECT capital
+   FROM countries)
+ORDER BY urbanarea_pop DESC;
+
+
+-- Subquery inside select
+-- Diese zwei Snippets machen das Gleiche
+
+SELECT countries.name AS country, COUNT(*) AS cities_num
+FROM cities
+INNER JOIN countries
+ON countries.code = cities.country_code
+GROUP BY country
+ORDER BY cities_num DESC, country
+LIMIT 9;
+ 
+SELECT countries.name AS country,
+  (SELECT COUNT(*)
+   FROM cities
+   WHERE countries.code = cities.country_code) AS cities_num
+FROM countries
+ORDER BY cities_num DESC, country
+LIMIT 9;
+
+
+-- Subquery inside from
+SELECT code, COUNT(*) AS lang_num
+FROM languages
+GROUP BY code;
+
+SELECT 	local_name, lang_num
+FROM countries,
+    (SELECT code, COUNT(*) AS lang_num
+     FROM languages
+     GROUP BY code) AS subquery
+WHERE countries.code = subquery.code
+ORDER BY lang_num DESC;
+
+
+
+-- Advanced subquery
+SELECT name, continent, inflation_rate
+FROM countries
+INNER JOIN economies
+USING(code)
+WHERE year = 2015;
+
+
+SELECT max(inflation_rate) AS max_inf
+FROM (
+    SELECT name, continent, inflation_rate
+    FROM countries
+    INNER JOIN economies
+    USING(code)
+    WHERE year = 2015) AS subquery
+GROUP BY continent;
+
+
+SELECT name, continent, inflation_rate
+FROM countries
+INNER JOIN economies 
+ON countries.code = economies.code
+WHERE year = 2015
+    AND inflation_rate IN (
+        SELECT max(inflation_rate) AS max_inf
+        FROM (
+            SELECT name, continent, inflation_rate
+            FROM countries
+            INNER JOIN economies
+            ON countries.code = economies.code
+            WHERE year = 2015) AS subquery
+            GROUP BY continent);
+			
+			
+			
+-- Subquery challenge
+SELECT code, inflation_rate, unemployment_rate
+FROM economies
+WHERE year = 2015 AND code NOT IN
+  (SELECT code
+   FROM countries
+   WHERE (gov_form = 'Constitutional Monarchy' OR gov_form LIKE '%Republic%'))
+ORDER BY inflation_rate;
+
+
