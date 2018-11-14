494fee41712a30063c35c6268697e8e04cbaf415
Georgits <georgi.tsertsvadze@gmail.com>
2018-11-14 22:50:39 +0100

    course 23, chapter 1

--- 02_Python/23_Building_Chatbots_in_Python/23_Building_Chatbots_in_Python.py
+++ 02_Python/23_Building_Chatbots_in_Python/23_Building_Chatbots_in_Python.py
@@ -247,3 +247,194 @@ send_message("do you remember your last birthday")
 send_message("do you think humans should be worried about AI")
 send_message("I want a robot friend")
 send_message("what if you could be anything you wanted")
+
+
+
+
+
+
+
+# Chapter 2: Understanding natural language
+import re
+# Intent classification with regex I
+keywords = {'thankyou': ['thank', 'thx'], 'goodbye': ['bye', 'farewell'], 'greet': ['hello', 'hi', 'hey']}
+
+responses = {
+        'goodbye': ['bye', 'farewell'],
+        'greet': ['hello', 'hi', 'hey'],
+        'thankyou': ['thank', 'thx']
+        }
+
+bot_template = "BOT : {0}"
+user_template = "USER : {0}"
+
+def send_message(message):
+    print(user_template.format(message))
+    response = respond(message)
+    print(bot_template.format(response))
+
+# Define a dictionary of patterns
+patterns = {}
+
+# Iterate over the keywords dictionary
+for intent, keys in keywords.items():
+    # Create regular expressions and compile them into pattern objects
+    patterns[intent] = re.compile('|'.join(keys))
+    
+# Print the patterns
+print(patterns)
+
+
+
+# Intent classification with regex II
+# Define a function to find the intent of a message
+def match_intent(message):
+    matched_intent = None
+    for intent, pattern in patterns.items():
+        # Check if the pattern occurs in the message 
+        if pattern.search(message):
+            matched_intent = intent
+    return matched_intent
+
+# Define a respond function
+def respond(message):
+    # Call the match_intent function
+    intent = match_intent(message)
+    # Fall back to the default response
+    key = "default"
+    if intent in responses:
+        key = intent
+    return responses[key]
+
+# Send messages
+send_message("hello!")
+send_message("bye byeee")
+send_message("thanks very much!")
+
+
+
+# Entity extraction with regex
+# Define find_name()
+def find_name(message):
+    name = None
+    # Create a pattern for checking if the keywords occur
+    name_keyword = re.compile('name|call')
+    # Create a pattern for finding capitalized words
+    name_pattern = re.compile('[A-Z]{1}[a-z]*')
+    if name_keyword.search(message):
+        # Get the matching words in the string
+        name_words = name_pattern.findall(message)
+        if len(name_words) > 0:
+            # Return the name if the keywords are present
+            name = ' '.join(name_words)
+    return name
+
+# Define respond()
+def respond(message):
+    # Find the name
+    name = find_name(message)
+    if name is None:
+        return "Hi there!"
+    else:
+        return "Hello, {0}!".format(name)
+
+# Send messages
+send_message("my name is David Copperfield")
+send_message("call me Ishmael")
+send_message("People call me Cassandra")
+
+
+
+
+#word vectors with spaCy
+# The user utterances are available in the list sentences, and the corresponding intents in labels.
+
+
+
+sentences = [' i want to fly from boston at 838 am and arrive in denver at 1110 in the morning',
+ ' what flights are available from pittsburgh to baltimore on thursday morning',
+ ' what is the arrival time in san francisco for the 755 am flight leaving washington',
+ ' cheapest airfare from tacoma to orlando',
+ ' round trip fares from pittsburgh to philadelphia under 1000 dollars',
+ ' i need a flight tomorrow from columbus to minneapolis',
+ ' what kind of aircraft is used on a flight from cleveland to dallas',
+ ' show me the flights from pittsburgh to los angeles on thursday',
+ ' all flights from boston to washington',
+ ' what kind of ground transportation is available in denver',
+ ' show me the flights from dallas to san francisco',
+ ' show me the flights from san diego to newark by way of houston',
+ ' what is the cheapest flight from boston to bwi',
+ ' all flights to baltimore after 6 pm',
+ ' show me the first class fares from boston to denver',
+ ' show me the ground transportation in denver',
+ ' all flights from denver to pittsburgh leaving after 6 pm and before 7 pm',
+ ' i need information on flights for tuesday leaving baltimore for dallas dallas to boston and boston to baltimore',
+ ' please give me the flights from boston to pittsburgh on thursday of next week',
+ ' i would like to fly from denver to pittsburgh on united airlines',
+ ' show me the flights from san diego to newark',
+ ' please list all first class flights on united from denver to baltimore',
+ ' what kinds of planes are used by american airlines',
+ " i'd like to have some information on a ticket from denver to pittsburgh and atlanta",
+ " i'd like to book a flight from atlanta to denver",
+ ' which airline serves denver pittsburgh and atlanta',
+ " show me all flights from boston to pittsburgh on wednesday of next week which leave boston after 2 o'clock pm",
+ ' atlanta ground transportation',
+ ' i also need service from dallas to boston arriving by noon',
+ ' show me the cheapest round trip fare from baltimore to dallas']
+
+
+labels = ['atis_flight',
+ 'atis_flight',
+ 'atis_flight_time',
+ 'atis_airfare',
+ 'atis_airfare',
+ 'atis_flight',
+ 'atis_aircraft',
+ 'atis_flight',
+ 'atis_flight',
+ 'atis_ground_service',
+ 'atis_flight',
+ 'atis_flight',
+ 'atis_flight',
+ 'atis_flight',
+ 'atis_airfare',
+ 'atis_ground_service',
+ 'atis_flight',
+ 'atis_flight',
+ 'atis_flight',
+ 'atis_flight',
+ 'atis_flight',
+ 'atis_flight',
+ 'atis_aircraft',
+ 'atis_airfare',
+ 'atis_flight',
+ 'atis_airline',
+ 'atis_flight',
+ 'atis_ground_service',
+ 'atis_flight',
+ 'atis_airfare']
+
+# Load the spacy model: nlp
+import spacy
+
+# https://github.com/explosion/spaCy/issues/1651
+nlp = spacy.load('en_core_web_md')
+
+# Calculate the length of sentences
+n_sentences = len(sentences)
+
+# Calculate the dimensionality of nlp
+embedding_dim = nlp.vocab.vectors_length
+
+
+# Initialize the array with zeros: X
+X = np.zeros((n_sentences, embedding_dim))
+
+# Iterate over the sentences
+for idx, sentence in enumerate(sentences):
+    # Pass each each sentence to the nlp object to create a document
+    doc = nlp(sentence)
+    # Save the document's .vector attribute to the corresponding row in X
+    X[idx, :] = doc.vector
+
+
