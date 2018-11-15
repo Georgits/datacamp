2e6283100122786b7bc1db7753bee2dd180856d7
Georgits <georgi.tsertsvadze@gmail.com>
2018-11-15 22:34:42 +0100

    course 23, chapter 2

--- 02_Python/23_Building_Chatbots_in_Python/23_Building_Chatbots_in_Python.py
+++ 02_Python/23_Building_Chatbots_in_Python/23_Building_Chatbots_in_Python.py
@@ -438,3 +438,142 @@ for idx, sentence in enumerate(sentences):
     X[idx, :] = doc.vector
 
 
+# Intent classification with sklearn
+# NICHT LAUFFÄHIG !!!!
+# Import SVC
+from sklearn.svm import SVC
+
+# Create a support vector classifier
+clf = SVC(C = 1)
+
+# Fit the classifier using the training data
+clf.fit(X_train, y_train)
+
+# Predict the labels of the test set
+y_pred = clf.predict(X_test)
+
+# Count the number of correct predictions
+n_correct = 0
+for i in range(len(y_test)):
+    if y_pred[i] == y_test[i]:
+        n_correct += 1
+
+print("Predicted {0} correctly out of {1} test examples".format(n_correct, len(y_test)))
+
+
+
+
+# Using spaCy's entity recogniser
+# Define included entities
+include_entities = ['DATE', 'ORG', 'PERSON']
+
+# Define extract_entities()
+def extract_entities(message):
+    # Create a dict to hold the entities
+    ents = dict.fromkeys(include_entities)
+    # Create a spacy document
+    doc = nlp(message)
+    for ent in doc.ents:
+        if ent.label_ in include_entities:
+            # Save interesting entities
+            ents[ent.label_] = ent.text
+    return ents
+
+print(extract_entities('friends called Mary who have worked at Google since 2010'))
+print(extract_entities('people who graduated from MIT in 1999'))
+
+
+
+
+# Assigning roles using spaCy's parser
+# Create the document
+doc = nlp("let's see that jacket in red and some blue jeans")
+
+colors =  ['black', 'red', 'blue']
+items = ['shoes', 'handback', 'jacket', 'jeans']
+
+def entity_type(word):
+    _type = None
+    if word.text in colors:
+        _type = "color"
+    elif word.text in items:
+        _type = "item"
+    return _type
+
+# Iterate over parents in parse tree until an item entity is found
+def find_parent_item(word):
+    # Iterate over the word's ancestors
+    for parent in word.ancestors:
+        # Check for an "item" entity
+        if entity_type(parent) == "item":
+            return parent.text
+    return None
+
+# For all color entities, find their parent item
+def assign_colors(doc):
+    # Iterate over the document
+    for word in doc:
+        # Check for "color" entities
+        if entity_type(word) == "color":
+            # Find the parent
+            item =  find_parent_item(word)
+            print("item: {0} has color : {1}".format(item, word))
+
+# Assign the colors
+assign_colors(doc) 
+
+
+
+
+# Rasa NLU
+# Import necessary modules
+from rasa_nlu.converters import load_data
+from rasa_nlu.config import RasaNLUConfig
+from rasa_nlu.model import Trainer
+
+# Create args dictionary
+args = {'pipeline': "spacy_sklearn"}
+
+# Create a configuration and trainer
+config = RasaNLUConfig(cmdline_args = args)
+trainer = Trainer(config)
+
+# Load the training data
+training_data = load_data("./training_data.json")
+
+# Create an interpreter by training the model
+interpreter = trainer.train(training_data)
+
+# Try it out
+print(interpreter.parse("I'm looking for a Mexican restaurant in the North of town"))
+
+
+
+
+
+
+
+# Data-efficient entity recognition
+# Import necessary modules
+from rasa_nlu.config import RasaNLUConfig
+from rasa_nlu.model import Trainer
+
+pipeline = [
+    "nlp_spacy",
+    "tokenizer_spacy",
+    "ner_crf"
+]
+
+# Create a config that uses this pipeline
+config = RasaNLUConfig(cmdline_args = {"pipeline": pipeline})
+
+# Create a trainer that uses this config
+trainer = Trainer(config)
+
+# Create an interpreter by training the model
+interpreter = trainer.train(training_data)
+
+# Parse some messages
+print(interpreter.parse("show me Chinese food in the centre of town"))
+print(interpreter.parse("I want an Indian restaurant in the west"))
+print(interpreter.parse("are there any good pizza places in the center?"))
