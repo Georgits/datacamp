0c64c77d97b86befa7c912b96e16e98dc016dfe1
Georgits <georgi.tsertsvadze@gmail.com>
2018-11-24 12:41:32 +0100

    R purr course, chapter 1

--- 01_R/Foundations_of_Functional_Programming_with_purrr/Foundations_of_Functional_Programming_with_purrr.R
+++ 01_R/Foundations_of_Functional_Programming_with_purrr/Foundations_of_Functional_Programming_with_purrr.R
@@ -55,4 +55,40 @@ class(list_of_df[[1]])
 list_of_df
 
 
+# Subsetting lists
+# Load repurrrsive package, to get access to the wesanderson dataset
+library(repurrrsive)
+# Load wesanderson dataset
+data(wesanderson)
+# Get structure of first element in wesanderson
+str(wesanderson[1])
+# Get structure of GrandBudapest element in wesanderson
+str(wesanderson$GrandBudapest)
 
+
+
+# Subsetting list elements
+# Third element of the first wesanderson vector
+wesanderson[[1]][3]
+# Fourth element of the GrandBudapest wesanderson vector
+wesanderson$GrandBudapest[4]
+# Subset the first element of the sw_films data
+sw_films[[1]]
+# Subset the first element of the sw_films data, title column 
+sw_films[[1]]$title
+
+
+
+# map() argument alternatives
+# Map over wesanderson to get the length of each element
+map(wesanderson, length)
+# Map over wesanderson, and determine the length of each element
+map(wesanderson, ~length(.x))
+
+
+
+# map_*
+# Map over wesanderson and determine the length of each element
+map(wesanderson, length)
+# Create a numcolors column and fill with length of each wesanderson element
+data.frame(numcolors = map_dbl(wesanderson, ~length(.x)))
