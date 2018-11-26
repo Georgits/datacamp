1dc8185c04c1e171b1b66e90c56a0386784a1a6a
Georgits <georgi.tsertsvadze@gmail.com>
2018-11-26 17:47:33 +0100

    purrr course, chapter 2

--- 01_R/Foundations_of_Functional_Programming_with_purrr/Foundations_of_Functional_Programming_with_purrr.R
+++ 01_R/Foundations_of_Functional_Programming_with_purrr/Foundations_of_Functional_Programming_with_purrr.R
@@ -123,3 +123,88 @@ numlist <- list(1,2,3,4,5,6,7,8,9,10)
 
 # Iterate over the numlist 
 map(numlist, ~.x %>% sqrt() %>% sin())
+
+
+
+
+# Simulating Data with Purrr
+# List of sites north, east, and west
+sites <- list("north", "east", "west")
+
+# Create a list of dataframes, each with a years, a, and b column 
+list_of_df <-  map(sites,  
+                   ~data.frame(sites = .x,
+                               a = rnorm(mean = 5, n = 200, sd = (5/2)),
+                               b = rnorm(mean = 200, n = 200, sd = 15)))
+
+list_of_df
+
+
+
+
+# Map over the models to look at the relationship of a vs b
+list_of_df %>%
+  map(~ lm(a ~ b, data = .)) %>%
+  map(summary)
+
+
+
+# map_chr()
+# Pull out the director element of sw_films in a list and character vector
+map(sw_films, ~.x[["director"]])
+map_chr(sw_films, ~.x[["director"]])
+
+# Compare outputs when checking if director is George Lucas
+map(sw_films, ~.x[["director"]] == "George Lucas")
+map_lgl(sw_films, ~.x[["director"]] == "George Lucas")
+
+
+
+
+# map_dbl() and map_int()
+# Pull out episode_id element as list
+map(sw_films, ~.x[["episode_id"]])
+
+# Pull out episode_id element as double vector
+map_dbl(sw_films, ~.x[["episode_id"]])
+
+# Pull out episode_id element as integer vector
+map_int(sw_films, ~.x[["episode_id"]])
+
+
+
+
+# Simulating data with multiple inputs using map2()
+# List of 1 through 3
+means <- list(1,2,3)
+# Create sites list
+sites <- list("north", "west", "east")
+# Map over two arguments: sites and mu
+list_of_files_map2 <- map2(sites, means, ~data.frame(sites = .x,
+                                                     a = rnorm(mean = .y, n = 200, sd = (5/2))))
+list_of_files_map2
+
+
+
+
+# Simulating data 3+ inputs with pmap()
+sites <- list("north", "west", "east")
+means <- list(1,2,3)
+means2 <- list(0.5,1,1.5)
+sigma <- list(1,2,3)
+sigma2 <- list(0.5,1,1.5)
+
+# Create a master list, a list of lists
+pmapinputs <- list(sites = sites,  means = means, sigma = sigma, 
+                   means2 = means2, sigma2 = sigma2)
+
+# Map over the master list
+list_of_files_pmap <- pmap(pmapinputs, 
+                           function(sites, means, sigma, means2, sigma2) 
+                             data.frame(sites = sites,
+                                        a = rnorm(mean = means, n = 200, sd = sigma),
+                                        b = rnorm(mean = means2, n = 200, sd = sigma2)))
+
+list_of_files_pmap
+
+
