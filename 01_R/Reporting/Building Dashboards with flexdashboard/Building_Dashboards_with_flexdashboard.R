f013fd54454f3212db86437df526c479dfac30e8
Georgits <georgi.tsertsvadze@gmail.com>
2018-12-17 16:50:28 +0100

    Building_Dashboards_with_flexdashboard, chapter 1

---
+++ 01_R/Reporting/Building Dashboards with flexdashboard/Building_Dashboards_with_flexdashboard.R
@@ -0,0 +1,16 @@
+library(shinydashboard)
+
+header <- dashboardHeader()
+sidebar <- dashboardSidebar()
+body <- dashboardBody()
+
+# Create the UI using the header, sidebar, and body
+ui <- dashboardPage(header, sidebar, body)
+
+server <- function(input, output) {}
+
+shinyApp(ui, server)
+
+
+
+# Chaoter 1:  Dashboard Layouts
