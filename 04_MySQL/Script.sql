a781498d8d796adb09929b2c18f4e4a10f755ceb
Georgits <georgi.tsertsvadze@gmail.com>
2019-03-10 10:37:22 +0100

    Learning SQL: Chapter 11

--- 04_MySQL/Script.sql
+++ 04_MySQL/Script.sql
@@ -1290,3 +1290,194 @@ SELECT 60 num UNION ALL
 SELECT 70 num UNION ALL
 SELECT 80 num UNION ALL
 SELECT 90 num) tens;
+
+
+
+
+
+
+
+
+/* CHAPTER 11: CONDITIONAL LOGIC */
+SELECT c.cust_id, c.fed_id, c.cust_type_cd,
+	CONCAT(i.fname, ' ', i.lname) indiv_name,
+    b.name business_name
+    FROM customer c LEFT OUTER JOIN individual i
+    ON c.cust_id = i.cust_id
+    LEFT OUTER JOIN business b
+    ON c.cust_id = b.cust_id;
+    
+SELECT c.cust_id, c.fed_id, c.cust_type_cd,
+	CASE
+		WHEN c.cust_type_cd = 'I'
+			THEN CONCAT(i.fname, ' ', i.lname)
+		WHEN c.cust_type_cd = 'B'
+			THEN b.name
+		ELSE 'Unknown'
+        END name
+	FROM customer c LEFT OUTER JOIN individual i
+    ON c.cust_id = i.cust_id
+    LEFT OUTER JOIN business b
+    ON c.cust_id = b.cust_id;
+    
+    
+SELECT c.cust_id, c.fed_id, c.cust_type_cd,
+	CASE
+		WHEN c.cust_type_cd = 'I'
+			THEN 
+				(SELECT CONCAT(i.fname, ' ', i.lname)
+					FROM individual i
+                    WHERE i.cust_id = c.cust_id)
+		WHEN c.cust_type_cd = 'B'
+			THEN 
+				(SELECT b.name
+					FROM business b
+                    WHERE b.cust_id = c.cust_id)
+		ELSE 'Unknown'
+        END name
+	FROM customer c;
+    
+    
+    
+    SELECT YEAR(open_date) year, COUNT(*) how_many
+		FROM account
+        WHERE open_date > '1999-12-31'
+			AND open_date < '2006-01-01'
+		GROUP BY YEAR(open_date);
+        
+	
+SELECT 
+			SUM(CASE
+				WHEN EXTRACT(YEAR FROM open_date) = 2000 THEN 1
+				ELSE 0
+            END) year_2000,
+			SUM(CASE
+				WHEN EXTRACT(YEAR FROM open_date) = 2001 THEN 1
+				ELSE 0
+            END) year_2001,
+            SUM(CASE
+				WHEN EXTRACT(YEAR FROM open_date) = 2002 THEN 1
+				ELSE 0
+            END) year_2002,
+            SUM(CASE
+				WHEN EXTRACT(YEAR FROM open_date) = 2003 THEN 1
+				ELSE 0
+            END) year_2003,
+            SUM(CASE
+				WHEN EXTRACT(YEAR FROM open_date) = 2004 THEN 1
+				ELSE 0
+            END) year_2004,
+            SUM(CASE
+				WHEN EXTRACT(YEAR FROM open_date) = 2005 THEN 1
+				ELSE 0
+            END) year_2005
+            FROM account
+            WHERE open_date > '1999-12-31' AND open_date < '2006-0101';
+            
+            
+            
+SELECT CONCAT('ALERT! : Account #', a.account_id, 'Has Incorrect Balance!')
+	FROM account AS a
+    WHERE (a.avail_balance, a.pendng_balance) <>
+		(SELECT SUM(CASE
+						WHEN t.funds_avail_date > current_timestamp() THEN 0
+						WHEN t.txn_type_cd = 'DBT' THEN t.amount * -1
+                        ELSE t.amount
+						END), 
+				SUM(CASE
+						WHEN t.txn_type_cd = 'DBT' THEN t.amount * -1
+                        ELSE t.amount
+                        END)
+        FROM transaction t
+        WHERE t.account_id = a.account_id);
+
+
+/* FLAG */
+SELECT c.cust_id, c.fed_id, c.cust_type_cd,
+	CASE
+		WHEN EXISTS (SELECT 1 FROM account a 
+			WHERE a.cust_id = c.cust_id
+				AND a.product_cd = 'CHK') THEN 'Y'
+		ELSE 'N'
+	END has_checking,
+    CASE
+		WHEN EXISTS (SELECT 1 FROM account a 
+			WHERE a.cust_id = c.cust_id
+				AND a.product_cd = 'SAV') THEN 'Y'
+		ELSE 'N'
+	END has_savings
+FROM customer c;
+
+
+
+SELECT c.cust_id, c.fed_id, c.cust_type_cd,
+	CASE (SELECT COUNT(*) FROM account a
+		WHERE a.cust_id = c.cust_id)
+        WHEN 0 THEN 'NONE'
+        WHEN 1 THEN '1'
+        WHEN 2 THEn '2'
+        ELSE '3+'
+	END num_accounts
+FROM customer c;
+
+
+/* Division by Zero errors */
+SELECT a.cust_id, a.product_cd, a.avail_balance / 
+	CASE
+		WHEN prod_tots.tot_balance = 0 THEN 1
+		ELSE prod_tots.tot_balance
+	END percent_of_total
+    FROM account a INNER JOIN 
+		(SELECT a.product_cd, SUM(a.avail_balance) tot_balance
+			FROM account a
+            GROUP BY a.product_cd) prod_tots
+            ON a.product_cd = prod_tots.product_cd;
+
+
+/* Handling NULL Values */
+SELECT emp_id, fname, lname,
+	CASE
+		WHEN title is NULL THEN 'Unknown'
+        ELSE title
+	END title
+FROM employee;
+
+
+/*
+SELECT <some calculation> + 
+	CASE
+		WHEN avail_balance IS NULL THEN 0
+		ELSE avail_balance
+	END
+    + <rest of calculation>
+*/
+
+
+/* EXERCISE 11-1 */
+SELECT emp_id,
+	CASE 
+		WHEN title LIKE '%President' OR title = 'Loan Manager' OR title = 'Treasurer' THEN 'Management'
+        WHEN title LIKE '%Teller' OR title = 'Operations Manager' THEN 'Operations'
+        ELSE 'Unknown'
+	END
+FROM employee;
+
+/* EXERCISE 11-2 */
+SELECT 
+	SUM(CASE
+		WHEN open_branch_id = 1 THEN 1
+        ELSE 0
+        END) branch_1,
+	SUM(CASE
+		WHEN open_branch_id = 2 THEN 1
+        ELSE 0
+        END) branch_2,
+	SUM(CASE
+		WHEN open_branch_id = 3 THEN 1
+        ELSE 0
+        END) branch_3,
+	SUM(CASE
+		WHEN open_branch_id = 4 THEN 1
+        ELSE 0
+        END) branch_4
+FROM account;
