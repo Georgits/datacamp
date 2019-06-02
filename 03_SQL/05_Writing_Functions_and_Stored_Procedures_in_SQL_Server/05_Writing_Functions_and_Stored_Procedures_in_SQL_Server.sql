bc25bf1aaf83c24cb419e5741df014f14f98217d
Georgits <georgi.tsertsvadze@gmail.com>
2019-06-02 23:19:48 +0200

    05_Writing_Functions_and_Stored_Procedures_in_SQL_Server: Chapter 1

---
+++ 03_SQL/05_Writing_Functions_and_Stored_Procedures_in_SQL_Server/05_Writing_Functions_and_Stored_Procedures_in_SQL_Server.sql
@@ -0,0 +1,134 @@
+-- Transactions per day
+SELECT
+  -- Select the date portion of StartDate
+  CONVERT(DATE, StartDate) as StartDate,
+  -- Measure how many records exist for each StartDate
+  COUNT(ID) as CountOfRows 
+FROM CapitalBikeShare 
+-- Group by the date portion of StartDate
+GROUP BY CONVERT(DATE, StartDate)
+-- Sort the results by the date portion of StartDate
+ORDER BY CONVERT(DATE, StartDate);
+
+
+
+-- Seconds or no seconds?
+SELECT
+	-- Count the number of IDs
+	COUNT(ID) AS Count,
+    -- Use DATEPART() to evaluate the SECOND part of StartDate
+    "StartDate" = CASE WHEN DATEPART(SECOND, StartDate) = 0 THEN 'SECONDS = 0'
+					  WHEN DATEPART(SECOND, StartDate) > 0 THEN 'SECONDS > 0' END
+FROM CapitalBikeShare
+GROUP BY
+    -- Complete the CASE statement
+	CASE WHEN DATEPART(SECOND, StartDate) = 0 THEN 'SECONDS = 0'
+		WHEN DATEPART(SECOND, StartDate) > 0 THEN 'SECONDS > 0' END
+		
+
+		
+		
+-- Which day of week is busiest?
+SELECT
+    -- Select the day of week value for StartDate
+	DATENAME(WEEKDAY, StartDate) as DayOfWeek,
+    -- Calculate TotalTripHours
+	SUM(DATEDIFF(SECOND, StartDate, EndDate))/ 3600 as TotalTripHours 
+FROM CapitalBikeShare 
+-- Group by the day of week
+GROUP BY DATENAME(WEEKDAY, StartDate)
+-- Order TotalTripHours in descending order
+ORDER BY TotalTripHours DESC
+
+
+
+
+-- Find the outliers
+SELECT
+	-- Calculate TotalRideHours using SUM() and DATEDIFF()
+  	SUM(DATEDIFF(SECOND, StartDate, EndDate))/ 3600 AS TotalRideHours,
+    -- Select the DATE portion of StartDate
+  	CONVERT(DATE, StartDate) AS DateOnly,
+    -- Select the WEEKDAY
+  	DATENAME(WEEKDAY, CONVERT(DATE, StartDate)) AS DayOfWeek 
+FROM CapitalBikeShare
+-- Only include Saturday
+WHERE DATENAME(WEEKDAY, StartDate) = 'Saturday' 
+GROUP BY CONVERT(DATE, StartDate);
+
+
+
+
+
+-- DECLARE & CAST
+-- Create @ShiftStartTime
+DECLARE @ShiftStartTime AS time = '08:00 AM'
+
+-- Create @StartDate
+DECLARE @StartDate AS date
+
+-- Set StartDate to the first StartDate from CapitalBikeShare
+SET 
+	@StartDate = (
+    	SELECT TOP 1 StartDate 
+    	FROM CapitalBikeShare 
+    	ORDER BY StartDate ASC
+		)
+
+-- Create ShiftStartDateTime
+DECLARE @ShiftStartDateTime AS datetime
+
+-- Cast StartDate and ShiftStartTime to datetime data types
+SET @ShiftStartDateTime = CAST(@StartDate AS datetime) + CAST(@ShiftStartTime AS datetime) 
+
+SELECT @ShiftStartDateTime
+
+
+
+
+-- DECLARE a TABLE
+-- Create @Shifts
+DECLARE @Shifts TABLE(
+    -- Create StartDateTime column
+	StartDateTime datetime,
+    -- Create EndDateTime column
+	EndDateTime datetime)
+-- Populate @Shifts
+INSERT INTO @Shifts (StartDateTime, EndDateTime)
+	SELECT '3/1/2018 8:00 AM', '3/1/2018 4:00 PM'
+SELECT * 
+FROM @Shifts
+
+
+
+-- INSERT INTO @TABLE
+-- Create @RideDates
+DECLARE @RideDates TABLE(
+    -- Create RideStart
+	RideStart date,
+    -- Create RideEnd
+	RideEnd date)
+-- Populate @RideDates
+INSERT INTO @RideDates(RideStart, RideEnd)
+-- Select the unique date values of StartDate and EndDate
+SELECT DISTINCT
+    -- Cast StartDate as date
+	CAST(StartDate AS date),
+    -- Cast EndDate as date
+	CAST(EndDate AS date) 
+FROM CapitalBikeShare 
+SELECT * 
+FROM @RideDates;
+
+
+
+
+-- Parameters matter with DATEDIFF
+SELECT DATEDIFF(day, '2/26/2018', '3/3/2018')
+SELECT DATEDIFF(week, '2/26/2018', '3/3/2018')
+SELECT DATEDIFF(month, '2/26/2018', '3/3/2018')
+
+
+-- First day of month
+-- Find the first day of the current month
+SELECT DATEADD(month, DATEDIFF(month, 0, GETDATE()), 0)
