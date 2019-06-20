64f25f30801dbf72fc40d65d30c4c419bb6f90ae
Georgits <georgi.tsertsvadze@gmail.com>
2019-06-20 18:28:25 +0200

    07_Data_Driven_Decision_Making_in_SQL: Chapter 3

--- 03_SQL/07_Data_Driven_Decision_Making_in_SQL/07_Data_Driven_Decision_Making_in_SQL.sql
+++ 03_SQL/07_Data_Driven_Decision_Making_in_SQL/07_Data_Driven_Decision_Making_in_SQL.sql
@@ -230,3 +230,213 @@ LEFT JOIN movies AS m
 ON m.movie_id = r.movie_id
 WHERE date_renting >= '2019-01-01'
 GROUP BY country;
+
+
+
+-- CHAPTER 3: Data Driven Decision Making with advanced SQL queries
+-- Often rented movies
+SELECT movie_id -- Select movie IDs with more than 5 views
+FROM renting
+GROUP BY movie_id
+HAVING COUNT(*) > 5;
+
+SELECT *
+FROM movies
+WHERE movie_id IN  -- Select movie IDs from the inner query
+	(SELECT movie_id
+	FROM renting
+	GROUP BY movie_id
+	HAVING COUNT(*)>5);
+    
+-- Frequent customers
+SELECT *
+FROM customers
+WHERE customer_id IN -- Select all customers with more than 10 movie rentals
+	(SELECT customer_id
+	FROM renting
+	GROUP BY customer_id
+	HAVING COUNT(*) > 10);
+    
+-- Movies with rating above average
+SELECT AVG(rating) -- Calculate the total average rating
+FROM renting;
+
+SELECT movie_id, -- Select movie IDs and calculate the average rating 
+       AVG(rating)
+FROM renting
+GROUP BY movie_id
+HAVING AVG(rating) >           -- Of movies with rating above average
+	(SELECT AVG(rating)
+	FROM renting);
+    
+SELECT title -- Report the movie titles of all movies with average rating higher than the total average
+FROM movies
+WHERE movie_id IN
+	(SELECT movie_id
+	 FROM renting
+     GROUP BY movie_id
+     HAVING AVG(rating) > 
+		(SELECT AVG(rating)
+		 FROM renting));
+
+-- Analyzing customer behavior
+-- Count movie rentals of customer 45
+SELECT COUNT(*)
+FROM renting
+WHERE customer_id = 45;
+
+-- Select customers with less than 5 movie rentals
+SELECT *
+FROM customers as c
+WHERE 5 > 
+	(SELECT count(*)
+	FROM renting as r
+	WHERE r.customer_id = c.customer_id);
+    
+    
+-- Customers who gave low ratings
+-- Calculate the minimum rating of customer with ID 7
+SELECT MIN(CAST(rating AS DECIMAL))
+FROM renting
+WHERE customer_id = 7;
+
+SELECT *
+FROM customers AS c
+WHERE 4 >  -- Select all customers with a minimum rating smaller than 4 
+	(SELECT MIN(rating)
+	FROM renting AS r
+	WHERE r.customer_id = c.customer_id);
+    
+    
+-- Movies and ratings with correlated queries
+SELECT *
+FROM movies AS m
+WHERE 5 <  -- Select all movies with more than 5 ratings
+	(SELECT COUNT(rating)
+	FROM renting AS r
+	WHERE m.movie_id = r.movie_id);
+    
+SELECT *
+FROM movies AS m
+WHERE 8 <  -- Select all movies with an average rating higher than 8
+	(SELECT AVG(CAST(rating AS DECIMAL))
+	FROM renting AS r
+	WHERE r.movie_id = m.movie_id AND  r.rating>0);
+    
+    
+-- Customers with at least one rating
+SELECT *
+FROM renting
+WHERE rating IS NOT NULL -- Exclude those with null ratings
+AND customer_id = 115;
+
+SELECT *
+FROM renting
+WHERE rating IS NOT NULL -- Exclude null ratings
+AND customer_id = 1; -- Select all ratings from customer with ID 1
+
+SELECT *
+FROM customers AS c -- Select all customers with at least one rating
+WHERE EXISTS
+	(SELECT *
+	FROM renting AS r
+	WHERE rating IS NOT NULL 
+	AND r.customer_id = c.customer_id);
+    
+    
+-- Actors in comedies
+SELECT *
+FROM actsin AS ai
+LEFT JOIN movies AS m
+ON m.movie_id = ai.movie_id
+WHERE m.genre = 'Comedy'
+AND ai.actor_id = 1; -- Select only the actor with ID 1
+
+
+SELECT *
+FROM actors AS a
+WHERE EXISTS
+	(SELECT *
+	 FROM actsin AS ai
+	 LEFT JOIN movies AS m
+	 ON m.movie_id = ai.movie_id
+	 WHERE m.genre = 'Comedy'
+	 AND ai.actor_id = a.actor_id);
+     
+     
+SELECT a.nationality, COUNT(*) -- Report the nationality and the number of actors for each nationality
+FROM actors AS a
+WHERE EXISTS
+	(SELECT ai.actor_id
+	 FROM actsin AS ai
+	 LEFT JOIN movies AS m
+	 ON m.movie_id = ai.movie_id
+	 WHERE m.genre = 'Comedy'
+	 AND ai.actor_id = a.actor_id)
+GROUP BY a.nationality;
+
+
+-- Young actors not coming from the USA
+SELECT name,  -- Report the name, nationality and the year of birth
+       nationality, 
+       year_of_birth
+FROM actors
+WHERE nationality <> 'USA'; -- Of all actors who are not from the USA
+
+SELECT name, 
+       nationality, 
+       year_of_birth
+FROM actors
+WHERE year_of_birth > 1990; -- Born after 1990
+
+
+SELECT name, 
+       nationality, 
+       year_of_birth
+FROM actors
+WHERE nationality <> 'USA'
+UNION  -- Select all actors who are not from the USA and all actors who are born after 1990
+SELECT name, 
+       nationality, 
+       year_of_birth
+FROM actors
+WHERE year_of_birth > 1990;
+
+
+SELECT name, 
+       nationality, 
+       year_of_birth
+FROM actors
+WHERE nationality <> 'USA'
+INTERSECT -- Select all actors who are not from the USA and who are also born after 1990
+SELECT name, 
+       nationality, 
+       year_of_birth
+FROM actors
+WHERE year_of_birth > 1990;
+
+
+-- Dramas with high ratings
+SELECT movie_id -- Select the IDs of all dramas
+FROM movies
+WHERE genre = 'Drama';
+
+SELECT movie_id -- Select the IDs of all movies with average rating higher than 9
+FROM renting
+GROUP BY movie_id
+HAVING AVG(rating) > 9;
+
+SELECT *
+FROM movies
+WHERE movie_id IN -- Select all movies of genre drama with average rating higher than 9
+   (SELECT movie_id
+    FROM movies
+    WHERE genre = 'Drama'
+    INTERSECT
+    SELECT movie_id
+    FROM renting
+    GROUP BY movie_id
+    HAVING AVG(rating)>9);
+    
+    
+    
